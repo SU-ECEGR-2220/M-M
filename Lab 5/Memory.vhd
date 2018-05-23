18899e6f58a5aff3d8ae873d345606c041e6e306
serenityengineer <38222989+serenityengineer@users.noreply.github.com>
2018-05-23 16:26:16 -0700

    ADDED REG

--- Lab 5/Memory.vhd
+++ Lab 5/Memory.vhd
@@ -117,10 +117,85 @@ end remember;
 			8 BIT REGISTER
 --------------------------------------------------------------------------
 
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
 
+entity register8 is
+	port(datain: in std_logic_vector(7 downto 0);
+	     enout:  in std_logic;
+	     writein: in std_logic;
+	     dataout: out std_logic_vector(7 downto 0));
+end entity register8;
 
+architecture memmy of register8 is
+	--using bitstorage from above as a component	
+	component bitstorage
+		port(bitin: in std_logic;
+		 	 enout: in std_logic;
+		 	 writein: in std_logic;
+		 	 bitout: out std_logic);
+	end component;
+begin
+	-- insert your code here.
+	--Code: using generate to instantiate the register
+	
+	REG_GEN:
+		FOR r8 IN 0 TO 7 GENERATE
+			R0: bitstorage PORT MAP(datain(r8), enout, writein, dataout(r8));
+		END GENERATE;
+end architecture memmy;
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------
+---------------------------------------------------------------------------
 			32 BIT REGISTER
---------------------------------------------------------------------------
+---------------------------------------------------------------------------
+
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Use ieee.std_logic_unsigned.all;
+
+entity register32 is
+	port(datain: in std_logic_vector(31 downto 0);
+		 enout32,enout16,enout8: in std_logic;
+		 writein32, writein16, writein8: in std_logic;
+		 dataout: out std_logic_vector(31 downto 0));
+end entity register32;
+
+architecture biggermem of register32 is
+	-- hint: you'll want to put register8 as a component here 
+	-- so you can use it below
+COMPONENT register8		--this defines the 8bit register as a usable component
+		PORT(datain: IN std_logic_vector(7 DOWNTO 0);
+	    	enout:  IN std_logic;
+	     	writein: IN std_logic;
+	     	dataout: OUT std_logic_vector(7 DOWNTO 0));
+	END COMPONENT;
+	--enable created as a vector, used to enable the 4 components of the 32
+	--bit register.
+	SIGNAL 	enable: std_logic_vector(3 DOWNTO 0);
+	SIGNAL	writein: std_logic_vector(3 DOWNTO 0);
+begin
+	-- insert code here.
+	-- instantiating the register
+	-- creating the structure for each component, set enables
+	enable <= 	"0000" WHEN enout32 = '0' ELSE
+				"1100" WHEN enout32 = '1' AND enout16 = '0' ELSE
+				"1110" WHEN enout32 = '1' AND enout16 = '1' AND enout8 = '0' ELSE
+				"1111" WHEN enout32 = '1' AND enout16 = '1' AND enout8 = '1';
+	
+	writein <=	"1111" WHEN writein32 = '1' ELSE
+				"0011" WHEN writein32 = '0' AND writein16 = '1' ELSE
+				"0001" WHEN writein32 = '0' AND writein16 = '0' AND writein8 = '1' ELSE
+				"0000" WHEN writein32 = '0' AND writein16 = '0' AND writein8 = '0';
+
+	-- using component of 8 bit register; stack of 4 8Bits creates a 32Bit reg:
+R1: register8 PORT MAP(datain(7 DOWNTO 0), enable(0), writein(0), dataout(7 DOWNTO 0));
+R2: register8 PORT MAP(datain(15 DOWNTO 8), enable(1), writein(1), dataout(15 DOWNTO 8));
+R3: register8 PORT MAP(datain(23 DOWNTO 16), enable(2), writein(2), dataout(23 DOWNTO 16));
+R4: register8 PORT MAP(datain(31 DOWNTO 24), enable(3), writein(3), dataout(31 DOWNTO 24));
+
+
+end architecture biggermem;
